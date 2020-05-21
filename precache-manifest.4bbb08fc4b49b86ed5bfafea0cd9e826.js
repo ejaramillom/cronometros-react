e544b4c8c5902547811688892501e548de748a3d
ejaramillom <ejaramilo@hotmail.com>
2020-05-21 10:08:58 -0500

    Updates

---
+++ precache-manifest.4bbb08fc4b49b86ed5bfafea0cd9e826.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6249c4547001f07296751196b901a487",
+    "url": "/cronometros-react/index.html"
+  },
+  {
+    "revision": "73365fcb009eb38db66c",
+    "url": "/cronometros-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6bd1086de51372c3a126",
+    "url": "/cronometros-react/static/css/main.b3d3b355.chunk.css"
+  },
+  {
+    "revision": "73365fcb009eb38db66c",
+    "url": "/cronometros-react/static/js/2.26720744.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cronometros-react/static/js/2.26720744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd1086de51372c3a126",
+    "url": "/cronometros-react/static/js/main.3c2395b9.chunk.js"
+  },
+  {
+    "revision": "dc10f82812e904eb717e",
+    "url": "/cronometros-react/static/js/runtime-main.5e9ee7b1.js"
+  }
+]);
